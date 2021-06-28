34972e8957367d5e9078dc4dbefe45898c458d21
lenaios <75113784+lenaios@users.noreply.github.com>
2021-06-28 23:01:52 +0900

    Create TargetNumber.swift

---
+++ programmers/DFS/TargetNumber.swift
@@ -0,0 +1,28 @@
+
+/*
+https://programmers.co.kr/learn/courses/30/lessons/43165
+*/
+
+import Foundation
+
+var answer = 0
+var numbersArr = [Int]()
+
+func solution(_ numbers:[Int], _ target:Int) -> Int {
+    numbersArr = numbers
+    dfs(0, numbers[0], target)
+    dfs(0, -numbers[0], target)
+    return answer
+}
+
+func dfs(_ depth: Int, _ sum: Int, _ target: Int) {
+    if depth == numbersArr.count - 1 {
+        if sum == target {
+            answer += 1
+        }
+        return
+    }
+    dfs(depth + 1, sum + numbersArr[depth + 1], target)
+    dfs(depth + 1, sum - numbersArr[depth + 1], target)
+}
+
