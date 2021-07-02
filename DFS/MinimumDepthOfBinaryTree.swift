785e238582010fa826583680674b1dd85edcc1f3
lenaios <75113784+lenaios@users.noreply.github.com>
2021-07-02 13:45:38 +0900

    Create MinimumDepthOfBinaryTree.swift

---
+++ DFS/MinimumDepthOfBinaryTree.swift
@@ -0,0 +1,35 @@
+
+/*
+https://leetcode.com/problems/minimum-depth-of-binary-tree
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func minDepth(_ root: TreeNode?) -> Int {
+        if root == nil { return 0 }
+        if root?.left == nil {
+            return minDepth(root?.right) + 1
+        }
+        if root?.right == nil {
+            return minDepth(root?.left) + 1
+        }
+        let left = minDepth(root?.left)
+        let right = minDepth(root?.right)
+        return min(left, right) + 1
+    }
+}
+
