311c430637c436ff2a95729d9e8aefa3c0d920bb
lenaios <75113784+lenaios@users.noreply.github.com>
2021-07-02 16:26:19 +0900

    Create BinarySearchTreeToGreaterSumTree.swift

---
+++ DFS/BinarySearchTreeToGreaterSumTree.swift
@@ -0,0 +1,36 @@
+
+/*
+https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    var queue: [Int] = [0]
+    func bstToGst(_ root: TreeNode?) -> TreeNode? {
+        dfs(root)
+        return root
+    }
+    
+    func dfs(_ node: TreeNode?) {
+        if node == nil { return }
+        dfs(node?.right)
+        node!.val += queue.removeFirst()
+        queue.append(node!.val)
+        dfs(node?.left)
+    }
+}
+
