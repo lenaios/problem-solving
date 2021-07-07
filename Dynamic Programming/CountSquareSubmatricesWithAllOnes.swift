99dbb578ba89b8854d526dd42164bd06dbf098a4
lenaios <75113784+lenaios@users.noreply.github.com>
2021-07-08 00:15:48 +0900

    Create CountSquareSubmatricesWithAllOnes.swift

---
+++ Dynamic Programming/CountSquareSubmatricesWithAllOnes.swift
@@ -0,0 +1,20 @@
+/*
+https://leetcode.com/problems/count-square-submatrices-with-all-ones
+*/
+
+class Solution {
+    func countSquares(_ matrix: [[Int]]) -> Int {
+        var _matrix = matrix
+        for i in 0..<matrix.count {
+            for j in 0..<matrix[0].count {
+                if matrix[i][j] == 1, i > 0 && j > 0 {
+                    _matrix[i][j] = min(_matrix[i - 1][j], _matrix[i][j - 1], _matrix[i - 1][j - 1]) + _matrix[i][j]
+                }
+            }
+        }
+        
+        var answer = _matrix.flatMap { $0 }
+        return answer.reduce(0) { $0 + $1 }
+    }
+}
+
