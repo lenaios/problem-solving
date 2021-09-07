0323168888526ab425977697003922611c0a8288
lenaios <75113784+lenaios@users.noreply.github.com>
2021-09-07 23:00:20 +0900

    Create BinarySearch.swift

---
+++ Binary Search/BinarySearch.swift
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/binary-search
+*/
+
+import Foundation
+
+class Solution {
+  func search(_ nums: [Int], _ target: Int) -> Int {
+    var left = 0
+    var right = nums.count - 1
+    while left <= right {
+      let mid = left + (right - left) / 2
+      // mid = (right + left) / 2
+      if nums[mid] == target {
+        return mid
+      }
+      if nums[mid] < target {
+        left = mid + 1
+      } else {
+        right = mid - 1
+      }
+    }
+    return -1
+  }
+}
